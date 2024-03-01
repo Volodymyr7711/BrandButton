919d6935d0ff6d5bad0b772e0afb9e4cfffbee21
Volodymyr Mendyk <volodymyrmendyk70@gmail.com>
2024-03-01 10:18:29 +0100

    Initial Commit

---
+++ BrandButton/BrandButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrandButtonApp.swift
+//  BrandButton
+//
+//  Created by Volodymyr Mendyk on 01/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct BrandButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrandButton/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BrandButton
+//
+//  Created by Volodymyr Mendyk on 01/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
